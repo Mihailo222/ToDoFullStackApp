9404adf9f4b231a7c26c9d16c2b20a155d8ffea6
root <root@DESKTOP-IVOOBMS>
2024-04-30 11:42:16 +0200

    adding client post feature

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React,{Fragment} from 'react';
+import './App.css';
+
+//components
+import InputToDo from './components/InputToDo';
+
+
+function App() {
+  return (<Fragment>
+    
+    <div className='container'>
+    
+     <InputToDo/>
+
+    </div>
+    
+    
+    </Fragment>);
+}
+
+export default App;
